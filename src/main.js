61b78e5d21b83594f2c06fbfe28763112641db7d
David Sutherland <dsutherl42@gmail.com>
2016-03-24 16:51:40 -0700

    Added cleaning for mistaken auto

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ var fs = require('fs');
 var path = require('path');
 var zlib = require('zlib');
 var dir = process.argv[2];
+dir = "../Scouting Data";
+
 if(dir == null) {
   dir = "../from_tablets";
 }
@@ -28,6 +30,20 @@ backup_path += "-" + d_str + ".csv";
 //Get all the files from dir recursively
 var files = _getFiles(dir);
 
+//Check if any duplicates
+var f_obj = {};
+var new_files = [];
+for (var f_num = 0; f_num < files.length; ++f_num) {
+  var file_name = path.basename(files[f_num]);
+  if (file_name in f_obj) {
+    console.log("Duplicate file detected " + file_name);
+  } else {
+    f_obj[file_name] = files[f_num];
+    new_files.push(files[f_num]);
+  }
+}
+files = new_files;
+
 //Iterate over all files in the directory
 var match_data = [];
 for (var i=0; i<files.length; ++i) {
@@ -46,26 +62,73 @@ for (var i=0; i<files.length; ++i) {
   }
 }
 
+
 // Generate row names and array-ify the JSON data
 var expanded_data = _expandData(match_data);
 
-// Get all the headers and their data types from the data array
-var headers = _extractHeaders(expanded_data);
-var header_names = [];
-for(var k in headers) header_names.push(k);
+// Sanitize the input JSON for programmatic errors
+_sanitizeData(match_data);
+var sanitized_expanded_data = _expandData(match_data);
+
+write_csv(expanded_data,output_path.replace(".csv","_orig.csv"));
+write_csv(sanitized_expanded_data,output_path);
 
 // Convert the JSON to CSV and output the file
-json2csv({ data: expanded_data, fields: header_names }, function(err, csv) {
-  if (err) console.log(err);
-   console.log("Writing CSV to " + output_path)
+function write_csv(data,csv_output_path) {
+  // Get all the headers and their data types from the data array
+  var headers = _extractHeaders(data);
+  var header_names = [];
+  for(var k in headers) header_names.push(k);
+  
+  json2csv({ data: data, fields: header_names }, function(err, csv) {
+    if (err) console.log(err);
+     console.log("Writing CSV to " + csv_output_path);
    
-   fs.writeFile(output_path, csv, function(err) {
-    if (err) throw err;
-    console.log('Done writing CSV');
-   });
-   fs.writeFile(backup_path,csv);  
-});
+     fs.writeFile(csv_output_path, csv, function(err) {
+      if (err) throw err;
+      console.log('Done writing CSV');
+     });
+     fs.writeFile(backup_path,csv);  
+  });
+}
 
+function _sanitizeData(data) {
+  for (var i = 0; i < data.length; ++i) {
+    var match_obj = data[i];  
+    var actions_array = match_obj["actions"];
+    var auto_action_count = 0;
+    
+    // Check to see how many auto actions there are...
+    for (var action_num = 0; action_num < actions_array.length; ++action_num) {
+      var action_obj = actions_array[action_num];
+      for (var action_name in action_obj) {
+        var phase = action_obj[action_name]["phase"];
+        if (phase === "autonomous") {
+          auto_action_count++;
+        }
+      }
+    }
+    
+    // If we have more than 2 auto actions, let's assume everything after
+    // the first one is wrong and change them all
+    if (auto_action_count > 2) {
+      var auto_action_num = 0;
+      for (var action_num = 0; action_num < actions_array.length; ++action_num) {
+        var action_obj = actions_array[action_num];
+        for (var action_name in action_obj) {
+          var phase = action_obj[action_name]["phase"];
+          if (phase === "autonomous") {
+            if (auto_action_num > 0 && action_name != "reach") { 
+              action_obj[action_name]["phase"] = "teleop";
+            }
+            auto_action_num++;
+          }
+        }
+      }
+      match_obj["fixed_auto"] = 1;
+    }
+  }
+}
 function _getFiles(dir) {
   var dir_files = fs.readdirSync(dir);
   var files = [];
@@ -73,7 +136,7 @@ function _getFiles(dir) {
     var file_path = path.resolve(dir,dir_files[i]);
     if (fs.statSync(file_path).isDirectory()) {
       files.push.apply(files,_getFiles(file_path));
-    } else {
+    } else if(path.basename(file_path).match(/\.json$/)) {
       files.push(file_path);
     }
   }
